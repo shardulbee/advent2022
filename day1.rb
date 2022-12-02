2586990d5ff1d371e7c53d950b6079107c7d4c9a
Shardul Baral <shardul@baral.ca>
2022-12-01 23:32:32 -0800

    .

--- day1.rb
+++ day1.rb
@@ -1,30 +1,47 @@
-require_relative './input_reader'
+require_relative 'input_reader'
 
-def compute_part_one(reader)
-  reader.as_lines.map(&:to_i).sum
+def test_input
+  raw = <<-INPUT
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+  INPUT
+
+  raw.split("\n\n").map { _1.split("\n").map(&:to_i) }
 end
 
-def compute_part_two(reader)
-  acc = {}
-  frequency = 0
-  found = false
-
-  while !found
-    reader.as_lines.each do |change|
-      frequency += change.to_i
-
-      if acc.include?(frequency)
-        return frequency
-      end
-
-      acc[frequency] = true
-    end
-  end
-
-  return -1
+def prod_input
+  InputReader
+    .new(1)
+    .as_line
+    .split("\n\n")
+    .map { _1.split("\n").map(&:to_i) }
 end
 
-reader = InputReader.new(1)
+def compute_part_one(input)
+  max_elem = input
+    .each
+    .with_index(1)
+    .max_by { |items, _| items.sum }
+  max_elem[0].sum
+end
 
-puts "Part 1: #{compute_part_one(reader)}"
-puts "Part 2: #{compute_part_two(reader)}"
+def compute_part_two(input)
+  input.map(&:sum).sort[-3..].sum
+end
+
+puts "part 1 - test: #{compute_part_one(test_input)}"
+puts "part 1 - prod: #{compute_part_one(prod_input)}"
+puts "part 2 - test: #{compute_part_two(test_input)}"
+puts "part 2 - test: #{compute_part_two(prod_input)}"
---
+++ day2.ml
@@ -0,0 +1,114 @@
+type choice = Rock | Paper | Scissors
+type game = { opponent: choice ; mine: choice }
+type result = Win | Loss | Draw
+type game_two = { opponent: choice; result: result }
+
+let value choice = match choice with
+  | Rock -> 1
+  | Paper -> 2
+  | Scissors -> 3
+
+let result_of (game: game) = match (game.opponent, game.mine) with
+  | (Rock, Rock) -> Draw
+  | (Paper, Paper) -> Draw
+  | (Scissors, Scissors) -> Draw
+  | (Scissors, Paper) -> Loss
+  | (Scissors, Rock) -> Win
+  | (Rock, Paper) -> Win
+  | (Rock, Scissors) -> Loss
+  | (Paper, Rock) -> Loss
+  | (Paper, Scissors) -> Win
+
+let score game = match (result_of game) with
+  | Win -> 6 + (value game.mine)
+  | Draw -> 3 + (value game.mine)
+  | Loss -> 0 + (value game.mine)
+
+let lines_from_file ic =
+  let rec lines_from_file_aux ic acc = match (input_line ic) with
+    | exception End_of_file  -> List.rev acc
+    | line -> lines_from_file_aux ic (line :: acc) in
+  lines_from_file_aux ic []
+
+let one_for_two game2 =
+  let draw_func opponent = opponent in
+    let loss_func opponent = match opponent with
+      | Rock -> Scissors
+      | Paper -> Rock
+      | Scissors -> Paper in
+    let win_func opponent = match opponent with
+      | Rock -> Paper
+      | Paper -> Scissors
+      | Scissors -> Rock in
+      let mine_func = match game2.result with
+        | Draw -> draw_func
+        | Win -> win_func
+        | Loss -> loss_func in
+      { opponent = game2.opponent; mine = mine_func game2.opponent  }
+
+exception Invalid_input;;
+
+let parse_one line =
+  let of_mine mine = match mine with
+      | "X" -> Rock
+      | "Y" -> Paper
+      | "Z" -> Scissors
+      | _ -> failwith "Invalid Input" in
+
+    let of_opponent mine = match mine with
+        | "A" -> Rock
+        | "B" -> Paper
+        | "C" -> Scissors
+        | _ -> failwith "Invalid Input" in
+      let split = Array.of_list (Str.split (Str.regexp " ") line) in
+        { opponent = of_opponent split.(0); mine = of_mine split.(1) }
+
+let parse_two line =
+  let of_mine mine = match mine with
+      | "X" -> Loss
+      | "Y" -> Draw
+      | "Z" -> Win
+      | _ -> failwith "Invalid Input" in
+
+    let of_opponent mine = match mine with
+        | "A" -> Rock
+        | "B" -> Paper
+        | "C" -> Scissors
+        | _ -> failwith "Invalid Input" in
+      let split = Array.of_list (Str.split (Str.regexp " ") line) in
+        {opponent = of_opponent split.(0); result = of_mine split.(1)}
+
+
+let rec parse_lines parse_func lines = match lines with
+  | [] -> []
+  | (head :: tail) -> (parse_func head)::(parse_lines parse_func tail)
+
+let day_to_file day = "./data/day" ^ string_of_int day ^ ".data"
+
+let part_one =
+  let ic = open_in (day_to_file 2) in
+    let games = parse_lines parse_one (lines_from_file ic) in
+      let scores = List.map score games in
+        List.fold_left (+) 0 scores
+
+let part_one_test =
+  let games = parse_lines parse_one (["A Y"; "B X"; "C Z"]) in
+    let scores = List.map score games in
+      List.fold_left (+) 0 scores
+
+let part_two_test =
+  let games = parse_lines parse_two (["A Y"; "B X"; "C Z"]) in
+  let scores = List.map score (List.map (one_for_two) games) in
+  List.fold_left (+) 0 scores
+
+let part_two =
+  let ic = open_in (day_to_file 2) in
+    let games = parse_lines parse_two (lines_from_file ic) in
+    let scores = List.map score (List.map (one_for_two) games) in
+    List.fold_left (+) 0 scores
+
+let () =
+  print_endline ("Part 1 - test: " ^ string_of_int part_one_test);;
+  print_endline ("Part 1: " ^ string_of_int part_one);;
+  print_endline ("Part 2 - test: " ^ string_of_int part_two_test);;
+  print_endline ("Part 2: " ^ string_of_int part_two);;
